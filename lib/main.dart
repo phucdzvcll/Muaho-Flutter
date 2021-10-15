84f19738c2540e42806cf2f4bcd8c1bcb34a1f5e
phucdzvcll <phucdriver1@gmail.com>
2021-10-15 14:04:06 +0700

    add EasyLocalization (multi language)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:muaho/common/MyTheme.dart';
+import 'package:muaho/generated/locale_keys.g.dart';
+
+import 'generated/codegen_loader.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('vi')],
+        path: 'assets/translations',
+        startLocale: Locale('vi'),
+        fallbackLocale: Locale('vi'),
+        assetLoader: CodegenLoader(),
+        child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +27,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
       title: 'Flutter Demo',
       theme: MyTheme.lightTheme,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -60,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     testingConnectFirebase();
                   },
                   child: Text(
-                    "Less Important".toUpperCase(),
+                    LocaleKeys.btn_login.tr(),
                     style: Theme.of(context)
                         .textTheme
                         .button!
