eb65a470afae074ad6d5180e6091c1f6104b8e06
duongdt <doanthanhduong11031990@gmail.com>
2022-01-14 21:29:22 +0700

    change structure per feature

---
+++ lib/features/di.dart
@@ -0,0 +1,133 @@
+import 'package:app_links/app_links.dart';
+import 'package:get_it/get_it.dart';
+import 'package:muaho/features/address/address_info/bloc/address_bloc.dart';
+import 'package:muaho/features/address/create_address/bloc/create_address_bloc.dart';
+import 'package:muaho/features/cart_update_bloc/cart_update_bloc.dart';
+import 'package:muaho/features/deeplink/deeplink_handle_bloc.dart';
+import 'package:muaho/features/home/setting_page/bloc/setting_bloc.dart';
+import 'package:muaho/features/login/bloc/login_bloc.dart';
+import 'package:muaho/features/register/bloc/register_bloc.dart';
+import 'package:muaho/features/voucher_list/bloc/voucher_list_bloc.dart';
+
+import 'cart/bloc/cart_bloc.dart';
+import 'chat-support/bloc/chat_bloc.dart';
+import 'home/history/complete/bloc/order_history_complete_bloc.dart';
+import 'home/history/delivering/bloc/order_history_delivering_bloc.dart';
+import 'home/history/history_order_detail/bloc/order_detail_bloc.dart';
+import 'home/home_page/bloc/home_page_bloc.dart';
+import 'main/bloc/main_bloc.dart';
+import 'order/bloc/order_bloc.dart';
+import 'payment/bloc/payment_bloc.dart';
+import 'search/hot_search/bloc/hot_search_bloc.dart';
+import 'search/search_shop/bloc/search_shop_bloc.dart';
+import 'sign_in/bloc/sign_bloc_bloc.dart';
+
+void presentationDiConfig(GetIt injector) {
+  injector.registerFactory(
+    () => SignBloc(
+      signInUseCase: injector(),
+    ),
+  );
+
+  injector.registerFactory(
+      () => SearchShopBloc(getListShopBySearchUseCase: injector()));
+
+  injector
+      .registerFactory(() => HotSearchBloc(getHotSearchUseCase: injector()));
+
+  injector.registerFactoryParam<PaymentBloc, CartUpdateBloc, void>(
+    (cartUpdateBloc, _) => PaymentBloc(
+      cartUpdateBloc: cartUpdateBloc,
+      createOrderUseCase: injector(),
+      appEventBus: injector(),
+    ),
+  );
+
+  injector.registerFactoryParam<OrderBloc, CartUpdateBloc, void>(
+      (cartUpdateBloc, _) => OrderBloc(
+          getShopProductUseCase: injector(), cartUpdateBloc: cartUpdateBloc));
+
+  injector.registerFactory(
+    () => HomePageBloc(
+      bannerUseCase: injector(),
+      useCaseProductCategories: injector(),
+      appEventBus: injector(),
+      userStore: injector(),
+      eventBus: injector(),
+    ),
+  );
+
+  injector.registerFactory<CartUpdateBloc>(() => CartUpdateBloc(
+        cartStore: injector(),
+      ));
+
+  injector.registerFactory<CreateAddressBloc>(() => CreateAddressBloc(
+        appGeoLocator: injector(),
+        createAddressUseCase: injector(),
+      ));
+
+  injector.registerFactory(
+      () => OrderDetailBloc(getOrderDetailUseCase: injector()));
+
+  injector.registerFactory(
+    () => OrderHistoryDeliveringBloc(
+        getOrderHistoryDeliveryUseCase: injector(), appEventBus: injector()),
+  );
+
+  injector.registerFactory(() =>
+      OrderHistoryCompleteBloc(getOrderHistoryCompleteUseCase: injector()));
+
+  injector.registerFactory(() => ChatBloc(userStore: injector()));
+
+  injector.registerFactory(() => AddressBloc(
+        getListAddressInfoUseCase: injector(),
+      ));
+
+  injector.registerFactoryParam<CartBloc, CartUpdateBloc, void>(
+      (cartUpdateBloc, _) => CartBloc(
+            cartUpdateBloc: cartUpdateBloc,
+          ));
+
+  injector.registerLazySingleton(
+    () => AppLinks(onAppLink: (Uri uri, String stringUri) {}),
+  );
+
+  injector.registerLazySingleton(
+      () => DeeplinkHandleBloc(appLinks: injector())..add(InitDeeplinkEvent()));
+
+  injector.registerFactory(
+    () => LoginBloc(
+      loginEmailUseCase: injector(),
+      appEventBus: injector(),
+    ),
+  );
+  injector.registerFactory(
+    () => RegisterBloc(
+      registerEmailUseCase: injector(),
+      appEventBus: injector(),
+    ),
+  );
+
+  injector.registerFactory(
+    () => SettingBloc(
+      userStore: injector(),
+      appEventBus: injector(),
+      firebaseAuth: injector(),
+      currentMode: injector(),
+    ),
+  );
+  injector.registerFactory(
+    () => VoucherListBloc(
+      getVoucherListUseCase: injector(),
+    ),
+  );
+
+  injector.registerFactory(
+    () {
+      return MainBloc(
+        appEventBus: injector(),
+        currentMode: injector(),
+      );
+    },
+  );
+}
---
+++ lib/features/home/home_page/bloc/home_page_bloc.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:muaho/common/common.dart';
+import 'package:muaho/domain/domain.dart';
+import 'package:muaho/features/home/home_page/model/home_page_model.dart';
+import 'package:muaho/features/login/bloc/login_bloc.dart';
+
+part 'home_page_event.dart';
+part 'home_page_state.dart';
+
+class _GetUserNameEvent extends HomePageEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class HomePageBloc extends Bloc<HomePageEvent, HomePageState> {
+  final GetListProductCategoriesHomeUseCase useCaseProductCategories;
+  final GetListBannerUseCase bannerUseCase;
+  List<ProductCategoryHomeEntity> _productCategories = [];
+  List<SlideBannerEntity> _slideBannerEntity = [];
+  final AppEventBus appEventBus;
+  final UserStore userStore;
+  final AppEventBus eventBus;
+  StreamSubscription<LoginSuccessEventBus>? loginSuccessListen;
+
+  HomePageBloc({
+    required this.useCaseProductCategories,
+    required this.bannerUseCase,
+    required this.appEventBus,
+    required this.userStore,
+    required this.eventBus,
+  }) : super(HomePageInitial()) {
+    on<HomePageRequestEvent>((event, emit) async {
+      await _handleHomePageRequestEvent(emit);
+    });
+
+    on<ChangeCart>((event, emit) async {
+      HomePageSuccessState homePageSuccessState = HomePageSuccessState(
+        homePageModel: HomePageModel(
+            productCategories: _productCategories,
+            slideBannerEntity: _slideBannerEntity),
+      );
+
+      emit(homePageSuccessState);
+    });
+
+    on<_GetUserNameEvent>((event, emit) async {
+      var userName = await userStore.getUserName();
+      emit(UserNameState(userName: userName.defaultEmpty()));
+    });
+
+    loginSuccessListen =
+        eventBus.on<LoginSuccessEventBus>().listen((event) async {
+      this.add(_GetUserNameEvent());
+    });
+  }
+
+  Future _handleHomePageRequestEvent(Emitter<HomePageState> emit) async {
+    emit(HomePageLoading());
+
+    Either<Failure, ProductCategoriesHomeResults> productCategoriesResult =
+        await useCaseProductCategories.execute(EmptyInput());
+
+    Either<Failure, BannersResult> bannersResult =
+        await bannerUseCase.execute(EmptyInput());
+    _productCategories.clear();
+    _slideBannerEntity.clear();
+    if (productCategoriesResult.isSuccess) {
+      var endIndex = productCategoriesResult.success.listProductCategory.length;
+      _productCategories.addAll(productCategoriesResult
+          .success.listProductCategory
+          .sublist(0, endIndex > 8 ? 8 : endIndex));
+    }
+    if (bannersResult.isSuccess) {
+      _slideBannerEntity.addAll(bannersResult.success.listBanner);
+    }
+    HomePageSuccessState homePageSuccessState = HomePageSuccessState(
+      homePageModel: HomePageModel(
+          productCategories: _productCategories,
+          slideBannerEntity: _slideBannerEntity),
+    );
+
+    emit(homePageSuccessState);
+    this.add(_GetUserNameEvent());
+  }
+
+  @override
+  Future<void> close() {
+    loginSuccessListen?.cancel();
+    return super.close();
+  }
+}
